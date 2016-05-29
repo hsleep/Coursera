99707be49ff82bf031d943d6855703b1d9fbfcc5
Jaesang Kim <honeysleep@gmail.com>
2016-05-30 00:43:18 +0900

    Implemented HorizontalBoxBlur.parBlur

--- parprog1/scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ parprog1/scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -37,12 +37,11 @@ object HorizontalBoxBlurRunner {
 object HorizontalBoxBlur {
 
   /** Blurs the rows of the source image `src` into the destination image `dst`,
-   *  starting with `from` and ending with `end` (non-inclusive).
-   *
-   *  Within each row, `blur` traverses the pixels by going from left to right.
-   */
+    *  starting with `from` and ending with `end` (non-inclusive).
+    *
+    *  Within each row, `blur` traverses the pixels by going from left to right.
+    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
-    // TODO implement this method using the `boxBlurKernel` method
     from until end foreach { y =>
       0 until src.width foreach { x =>
         val blurredRgba = boxBlurKernel(src, x, y, radius)
@@ -52,15 +51,21 @@ object HorizontalBoxBlur {
   }
 
   /** Blurs the rows of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  rows.
-   */
+    *
+    *  Parallelization is done by stripping the source image `src` into
+    *  `numTasks` separate strips, where each strip is composed of some number of
+    *  rows.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-  // TODO implement using the `task` construct and the `blur` method
-
-  ???
+    val indices = 0.to(src.height, src.height / numTasks)
+    val parTasks = 0 until (numTasks - 1) map { numTask =>
+      val (from, end) = (indices(numTask), indices(numTask + 1))
+      task { blur(src, dst, from, end, radius)}
+    }
+    blur(src, dst, indices(numTasks - 1), src.height, radius)
+    parTasks.foreach { task =>
+      task.join()
+    }
   }
 
 }
